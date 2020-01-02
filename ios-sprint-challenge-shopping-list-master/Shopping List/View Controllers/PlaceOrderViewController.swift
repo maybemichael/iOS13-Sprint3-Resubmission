18c0156c5451b78c44592b331cceff7182f85345
Michael McGrath <maybemichael@icloud.com>
2020-01-02 15:10:55 -0800

    Completed MVP, starting on stretch goals.

--- ios-sprint-challenge-shopping-list-master/Shopping List/View Controllers/PlaceOrderViewController.swift
+++ ios-sprint-challenge-shopping-list-master/Shopping List/View Controllers/PlaceOrderViewController.swift
@@ -10,23 +10,34 @@ import UIKit
 
 class PlaceOrderViewController: UIViewController {
 
+    @IBOutlet weak var itemCountLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
     var itemCount: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func placeOrderTapped(_ sender: Any) {
+        guard let name = nameTextField.text, let address = addressTextField.text, !name.isEmpty, !address.isEmpty else { return }
+        
+        let orderAlert = UIAlertController(title: "Thank you \(name)!", message: "Your order will be delivered to \(address) in 15 minutes.", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .default) {
+            orderAlert in self.navigationController?.popToRootViewController(animated: true)
+        }
+        orderAlert.addAction(action)
+        present(orderAlert, animated: true)
     }
-    */
-
+    
+    
+    
+    func updateViews() {
+        guard let itemCount = itemCount else { return }
+        
+        itemCountLabel.text = "You currently have \(itemCount) item(s) in your shopping list."
+    }
 }
